9682edca991e6a739f5402fa29aa42bcdf3e138c
tatumvt <62714286+tatumvt@users.noreply.github.com>
2021-05-27 14:58:18 +0200

    geurspoor delay

--- VRTest/Assets/scripts/GameManager.cs
+++ VRTest/Assets/scripts/GameManager.cs
@@ -1,35 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static OVRInput;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject scent;
-
+    public Button endTurnBTN;
     void Update()
     {
         if (OVRInput.Get(OVRInput.Button.One))
         {
             scent.SetActive(true);
+            StartCoroutine(TimeoutEndTurnButton());
         }
         else
         {
             scent.SetActive(false);
         }
     }
-    public IEnumerator updateLoop()
+    public IEnumerator TimeoutEndTurnButton()
     {
         if (OVRInput.Get(OVRInput.Button.One))
         {
-            scent.SetActive(true);
-            yield return new WaitForSeconds(1f);
-            StartCoroutine(updateLoop());
-        } else
-            {
+            yield return new WaitForSeconds(5f);
             scent.SetActive(false);
             yield return new WaitForSeconds(1f);
-            StartCoroutine(updateLoop());
-            }
+            scent.SetActive(true);
         }
+      }
+
     }
-}
+
