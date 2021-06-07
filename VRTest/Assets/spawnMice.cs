46a207d3c5c781fc065057d8bbb0d3e1ae16d2b9
tatumvt <62714286+tatumvt@users.noreply.github.com>
2021-06-07 16:03:57 +0200

    mice ending trigger

--- VRTest/Assets/spawnMice.cs
+++ VRTest/Assets/spawnMice.cs
@@ -7,17 +7,14 @@ public class spawnMice : MonoBehaviour
 {
     public GameObject mice;
     public GameObject cat;
-    private void Start()
-    {
-        cat = GameObject.FindGameObjectWithTag("Cat");
-    }
+   
     private void OnTriggerEnter(Collider collider)
     {
         if (cat.activeSelf){
             if (collider.gameObject.tag == "Player")
             {
-                SceneManager.LoadScene(2);
                 mice.SetActive(true);
+                SceneManager.LoadScene(2);
             }
         }
     }
